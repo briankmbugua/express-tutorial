2bdf12180606e4b779eaa0cb38e027e14926896f
briankmbugua <briankmbuguak@gmail.com>
2022-02-19 23:14:21 +0300

    express server

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
-const http = require('http');
+const express = require('express');
+const app = express()
 
-const{readFileSync} = require('fs');
-//get all files
-const homePage = readFileSync('nav/final/index.html')
-const homeImage = readFileSync('nav/final/logo.svg')
-const homeStyles = readFileSync('nav/final/styles.css')
-const logic = readFileSync('nav/final/app.js')
+app.use(express.static('./public'))
 
+app.get('/',(req,res)=>{
 
-const server = http.createServer((req,res)=>{
-    const url = req.url;
-    if(url === '/'){
-        res.writeHead(200,{'content-type':'text/html'})//to provide meta data about our response 
-        res.write(homePage)
-        res.end()
-    }
-    //about page
-    else if(url === '/about') {
-        res.writeHead(200,{'content-type':'text/html'})//to provide meta data about our response 
-        res.write('<h1>About Page</h1>')
-        res.end()
-    }
-    //styles
-    else if(url === '/styles.css') {
-        res.writeHead(200,{'content-type':'text/css'})//to provide meta data about our response 
-        res.write(homeStyles)
-        res.end()
-    }
-    //image
-    else if(url === '/logo.svg') {
-        res.writeHead(200,{'content-type':'image/svg+xml'})//to provide meta data about our response 
-        res.write(homeImage)
-        res.end()
-    }
-    //lpgic
-    else if(url === '/logic.js') {
-        res.writeHead(200,{'content-type':'text/javascript'})//to provide meta data about our response 
-        res.write(logic)
-        res.end()
-    }
-    else {
-        res.writeHead(404,{'content-type':'text/html'})//to provide meta data about our response 
-        res.write('<h1>page not found</h1>')
-        res.end()
-    }
-}) //callback inside method createserver is called everytime our user hits the server
+})
 
-server.listen(5000)
+app.all('*',(req,res)=>{
+    res.status(404).send("resource not found")
+})
+
+app.listen(5000, ()=>{
+    console.log('sever is listening on port 5000....')
+})
