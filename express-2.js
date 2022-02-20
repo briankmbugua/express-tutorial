060b52048327354d7a5b8885b8635dab6a399a74
briankmbugua <briankmbuguak@gmail.com>
2022-02-21 00:57:59 +0300

    express servers

---
+++ express-2.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// setup static and middleware
+
+app.use(express.static('./public'))//static assets the ones server does not to change
+//are put in the public
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './nav/index.html'))
+//adding to static assets
+//SSR
+// })
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000....')
+})
