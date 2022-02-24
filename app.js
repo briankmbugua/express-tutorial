816eb4f67ff9487dda99383301e239ffbae9ade5
briankmbugua <briankmbuguak@gmail.com>
2022-02-24 22:26:09 +0300

    express middleware

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
 const express = require('express')
 const app = express()
-const {products} = require('./data')
-app.get('/', (req, res) => {
-  res.send('<h1>HomePage</h1><a href="/api/products">products</a>')
-})
-app.get('/api/products/:productID',(req,res)=>{
-    // console.log(req)
-    // console.log(req.params)
-    const{productID} = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productID))
-    if(!singleProduct) {
-        return res.status(404).send('Product does not exist')
-    }
-    return res.json(singleProduct) 
-})
+const logger = require('./logger')
+const authorize = require('./authorize')
+// req => middleware => res seats in bewtween
+app.use([logger,authorize]) //this is how you execute multiple
+//middleware functions,order also matters here
 
-app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
-    console.log(req.params)
-    res.send('hello world')
+app.get('/',(req,res)=>{
+res.send('Home')
 })
-
-app.get('/api/v1/query',(req,res)=>{
-    console.log(req.query);
-    const{search,limit} = req.query;
-    let sortedProducts = [...products];
-    if(search) {
-        sortedProducts = sortedProducts.filter((product)=>{
-            
-        })
-    }
-    res.send('hello worldd')
+app.get('/about',(req,res)=>{
+    res.send('About')
 })
-
-
+app.get('/api/products',(req,res)=>{
+    res.send('products')
+})
+app.get('/api/items',(req,res)=>{
+    res.send('Items')
+})
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
